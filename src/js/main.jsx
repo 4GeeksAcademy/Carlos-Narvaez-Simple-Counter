9eccaac26e12edb2603483f8810f90a0b9464667
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-03-26 19:49:38 +0000

    counter function

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,21 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let counter = 0;
+
+setInterval(() => {
+
+  let One = Math.floor((counter/1)%10)
+  let Two = Math.floor((counter/10)%10)
+  let Three = Math.floor((counter/100)%10)
+  let Four = Math.floor((counter/1000)%10)
+
+  counter++
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home digFour={Four} digThree={Three} digTwo={Two} digOne={One} />
   </React.StrictMode>,
 )
+  
+}, 1000);
